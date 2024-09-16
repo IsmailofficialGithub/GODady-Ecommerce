81e0c8a35fc8030d911e30c13d06c40aad1a984d
Ismail abbasi <ismail.official295@gmail.com>
2024-09-16 20:27:10 +0500

    prot

--- back-end/server.js
+++ back-end/server.js
@@ -30,7 +30,7 @@ app.use('/api/v1/product',ProductRoute)
 
 
 //port
-const PORT=process.env.PORT ;
+const PORT=process.env.PORT ||4500 ;
 app.listen(PORT,()=>{
     console.log(`server is running on ${process.env.DEV_MODE} at ${PORT} port`.bgGreen)
     
